136d39f8df6d3406841e68c0468ae1b3174b78d3
RWoolard <r.woolard456@gmail.com>
2016-05-12 17:11:24 -0400

    palindrome inspector

--- objc-palindrome-inspector/FISAppDelegate.m
+++ objc-palindrome-inspector/FISAppDelegate.m
@@ -8,7 +8,38 @@
     // Override point for customization after application launch.
     return YES;
 }
+- (BOOL) stringIsPalindrome:(NSString *)string {
+    
+    
+    NSArray *punctuation = @[@",", @".", @"?", @"!", @":", @";"];
+    NSString *withoutPunc = [string copy];
+    for (NSUInteger i = 0; i < [punctuation count]; i++) {
+        NSString *punctuations = punctuation[i];
+        withoutPunc = [withoutPunc stringByReplacingOccurrencesOfString:punctuations withString:@""];
+    }
+    NSString *spaceless = [withoutPunc stringByReplacingOccurrencesOfString:@" " withString:@""];
+    NSString *lowercase = [spaceless lowercaseString];
+    NSString *reverse = [self stringByReversingString:lowercase];
+                                  
+    BOOL stringIsEqualToReverse = [lowercase isEqualToString:reverse];
 
+
+    return stringIsEqualToReverse;
+}
+
+- (NSString *) stringByReversingString:(NSString *)string {
+    
+    NSString *result = @"";
+    
+    for (NSUInteger i = [string length]; i > 0; i--) {
+        NSUInteger index = i - 1;
+        unichar c = [string characterAtIndex:index];
+        result = [result stringByAppendingFormat:@"%c", c];
+    }
+
+    
+    return result;
+}
 /**
  
  * Implement your methods here.
